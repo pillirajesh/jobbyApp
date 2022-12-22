b256ef2be05b3a8e58e4568900444d2604a10741
pillirajesh <pillirajesh@ymail.com>
2022-12-22 20:03:01 +0000

    jobbyApp

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,4 +1,7 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+
 import './index.css'
 
 const employmentTypesList = [
@@ -40,8 +43,79 @@ const salaryRangesList = [
 ]
 
 class Jobs extends Component {
+  state = {profile: {}}
+
+  componentDidMount() {
+    this.getUserProfile()
+  }
+
+  getUserProfile = async () => {
+    const apiUrl = 'https://apis.ccbp.in/profile'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+    console.log(data)
+    const updatedData = data.profile_details
+    this.setState({profile: updatedData})
+  }
+
   render() {
-    return <div>Hi</div>
+    const {profile} = this.state
+    return (
+      <div className="jobs-container">
+        <Header />
+        <div>
+          <div className="container">
+            <div className="profile-container">
+              <img
+                src={profile.profile_image_url}
+                alt={profile.name}
+                className="profile-image"
+              />
+              <h1 className="profile-heading">{profile.name}</h1>
+              <p className="profile-paragraph">{profile.short_bio}</p>
+            </div>
+            <hr className="line" />
+            <h1 className="employment-type-heading">Employment Type</h1>
+            <ul className="employment-un-ordered-list-container ">
+              {employmentTypesList.map(eachJob => (
+                <li
+                  key={eachJob.employmentTypeId}
+                  className="employment-list-container"
+                >
+                  <input type="checkbox" id="jobType" />
+                  <label htmlFor="jobType" className="job">
+                    {eachJob.label}
+                  </label>
+                </li>
+              ))}
+            </ul>
+            <hr className="line" />
+            <h1 className="employment-type-heading">Salary Range</h1>
+            <ul className="employment-un-ordered-list-container ">
+              {salaryRangesList.map(eachSalary => (
+                <li
+                  key={eachSalary.salaryRangeId}
+                  className="employment-list-container"
+                >
+                  <input type="radio" id="salaryRange" />
+                  <label htmlFor="salaryRange" className="job">
+                    {eachSalary.label}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 export default Jobs
